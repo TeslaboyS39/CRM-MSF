582ca84a7f273eb1590710bd94c92d9f94bb2f2c
TeslaboyS39 <Fatahillah@yandex.com>
2026-01-27 09:41:25 +0700

    config return

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase Configuration
+// This file is gitignored - do not commit to repository
+const CONFIG = {
+    SUPABASE_URL: 'https://apntbvkdnjpvezyabzql.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwbnRidmtkbmpwdmV6eWFienFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3OTQ5NzEsImV4cCI6MjA4NDM3MDk3MX0.BnDmJJk2zPy1vp14rd9ceGk8Mk9cx7x35gRDH-Otezo'
+};
